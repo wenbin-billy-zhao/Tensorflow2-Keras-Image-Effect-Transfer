b166c30a72bb422192e931d9d5b77ba6585b8be3
Jamie Vo <vo.thipretty@gmail.com>
2019-07-02 01:15:37 -0500

    add in images

--- static/js/logic.js
+++ static/js/logic.js
@@ -9,31 +9,43 @@ async function pullData() {
 const artData = data
 
 // read data into the table
+
+// manipulate the array
 var art_arrays = []
 var art_pieces = []
 for (i = 0; i < artData.artist.length; i++) {
     artArray = [];
     artPiece = []
     artArray.push(artData.artist[i]);
-    artPiece.push(artData.art_image[i]);
     artArray.push(artData.art_title[i]);
+    artPiece.push(artData.art_image[i]);
     console.log(artData.artist[i]);
     art_arrays.push(artArray);
     art_pieces.push(artPiece)
 };
 console.log(art_arrays);
 
-
 const tbody = d3.select("tbody");
 // const tbody = metadata.append("tbody");
-
-
+var counter = 0;
+var counter2 = 0
     //  append to the table using a function to loop through all data
     art_arrays.forEach((product) =>{
     row = tbody.append("tr");
     for (key in product){
-        const cell = tbody.append("td");
+        counter = counter + 1
+        const cell = row.append("td");
+        row.attr("type", "radio")
+        row.attr("class", "style")
         cell.text(product[key]);
+        cell.attr('id', product[key]);
+        if (counter==2) {
+            cell.append("br")
+            const img = cell.append("img")
+            img.attr('src', art_pieces[counter2])
+            counter2 = counter2 + 1
+            counter = 0
+        }
     };
 });
 }
