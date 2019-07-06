b6716862f1a6a9d6fdd2b5941f1d6a321639d5f1
Suchita <coralmaven@yahoo.com>
2019-07-05 23:41:10 -0500

    The neuralStyleTransfer.ipynb now runs with function from stm_model.py

--- stm_model.py
+++ stm_model.py
@@ -3,39 +3,29 @@
 
 # import tensor flow for machine learning
 import tensorflow as tf
-import transfer_tools as tt
 from tensorflow.python.keras.preprocessing import image as kp_image
 from tensorflow.python.keras import Model, models, losses, layers
 from tensorflow.python.keras import backend as K
 from tensorflow.python.keras.models import model_from_json
+import tensorflow.contrib.eager as tfe
+
 from keras.layers import Dense, Flatten, Dropout
 from keras.callbacks import ModelCheckpoint
+
+import numpy as np
+from PIL import Image
+import time
+import functools
+import transfer_tools as tt
+
+import IPython.display
 
 class StyleTransferModel:
     
     ###########################################################################
 
     def __init__(self):
-        # Content layer where will pull our feature maps
-        content_layers = ['block5_conv2'] 
 
-        # Style layer we are interested in
-        style_layers = ['block1_conv1',
-                        'block2_conv1',
-                        'block3_conv1', 
-                        'block4_conv1', 
-                        'block5_conv1'
-                       ]
-        json_file = open("model.json", 'r')
-        loaded_model_json = json_file.read()
-        json_file.close()
-        vgg = model_from_json(loaded_model_json)
-        vgg.load_weights("model.h5")
-        style_outputs = [vgg.get_layer(name).output for name in style_layers]
-        content_outputs = [vgg.get_layer(name).output for name in content_layers]
-        model_outputs = style_outputs + content_outputs
-        self.model = models.Model(vgg.input, model_outputs)
-        
         """ Creates our model with access to intermediate layers. 
 
         This function will load the VGG19 model and access the intermediate layers. 
@@ -45,104 +35,85 @@ class StyleTransferModel:
         self.model is a keras model that takes image inputs and outputs the style and 
           content intermediate layers. 
         """
+
+        json_file = open("model.json", 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        model = model_from_json(loaded_model_json)
+        model.load_weights("model.h5")
+
+        '''The model reads the raw image as input pixels and builds an internal representation through
+        transformations that turn the pixels into a complex understanding of the features 
+        present within the image. Convolutional neural networks capture the invariances and defining 
+        features within classes (e.g., cats vs. dogs) that are agnostic to background noise and other
+        nuances. It serves as a complex feature extractor; We can access intermediate layers to describe
+        the content and style of input images.'''
+        
+        # Content layer where will pull our feature maps
+        self.content_layers = ['block5_conv2'] 
+
+        # Style layer we are interested in
+        self.style_layers = ['block1_conv1',
+                        'block2_conv1',
+                        'block3_conv1', 
+                        'block4_conv1', 
+                        'block5_conv1'
+                       ]
+
+        self.num_content_layers = len(self.content_layers)
+        self.num_style_layers = len(self.style_layers)
+                
+        self.style_outputs = [model.get_layer(name).output for name in self.style_layers]
+        self.content_outputs = [model.get_layer(name).output for name in self.content_layers]
+        
+        self.model_outputs = self.style_outputs + self.content_outputs
+        self.model = models.Model(model.input, self.model_outputs)
+        
   
-    def compute_loss(self, loss_weights, init_image, gram_style_features, content_features):
-        """This function will compute the loss total loss.
-
-        Arguments:
-        model: The model that will give us access to the intermediate layers
-        loss_weights: The weights of each contribution of each loss function. 
-          (style weight, content weight, and total variation weight)
-        init_image: Our initial base image. This image is what we are updating with 
-          our optimization process. We apply the gradients wrt the loss we are 
-          calculating to this image.
-        gram_style_features: Precomputed gram matrices corresponding to the 
-          defined style layers of interest.
-        content_features: Precomputed outputs from defined content layers of 
-          interest.
-
-        Returns:
-        returns the total loss, style loss, content loss, and total variational loss
-        """
-        style_weight, content_weight = loss_weights
-
-        # Feed our init image through our model. This will give us the content and 
-        # style representations at our desired layers. Since we're using eager
-        # our model is callable just like any other function!
-        model_outputs = self.model(init_image)
-
-        style_output_features = model_outputs[:num_style_layers]
-        content_output_features = model_outputs[num_style_layers:]
-
-        style_score = 0
-        content_score = 0
-
-        # Accumulate style losses from all layers
-        # Here, we equally weight each contribution of each loss layer
-        weight_per_style_layer = 1.0 / float(num_style_layers)
-        for target_style, comb_style in zip(gram_style_features, style_output_features):
-            style_score += weight_per_style_layer * tt.get_style_loss(comb_style[0], target_style)
-
-        # Accumulate content losses from all layers 
-        weight_per_content_layer = 1.0 / float(num_content_layers)
-        for target_content, comb_content in zip(content_features, content_output_features):
-            content_score += weight_per_content_layer* tt.get_content_loss(comb_content[0], target_content)
-
-        style_score *= style_weight
-        content_score *= content_weight
-
-        # Get total loss
-        loss = style_score + content_score 
-        return loss, style_score, content_score
-
-
-    def get_feature_representations(self, content_path, style_path):
-        """Helper function to compute our content and style feature representations.
-
-        This function will simply load and preprocess both the content and style 
-        images from their path. Then it will feed them through the network to obtain
-        the outputs of the intermediate layers. 
-
-        Arguments:
-        model: The model that we are using.
-        content_path: The path to the content image.
-        style_path: The path to the style image
-
-        Returns:
-        returns the style features and the content features. 
-        """
-        # Load our images in 
-        content_image = tt.load_and_process_img(content_path)
-        style_image = tt.load_and_process_img(style_path)
-
-        # batch compute content and style features
-        style_outputs = self.model(style_image)
-        content_outputs = self.model(content_image)
-
-
-        # Get the style and content feature representations from our model  
-        style_features = [style_layer[0] for style_layer in style_outputs[:num_style_layers]]
-        content_features = [content_layer[0] for content_layer in content_outputs[num_style_layers:]]
-        return style_features, content_features
-
  
     def run_style_transfer(self, content_path, 
                            style_path,
                            num_iterations=1000,
                            content_weight=1e3, 
                            style_weight=1e-2): 
-        # We don't need to (or want to) train any layers of our model, so we set their
-        # trainable to false. 
+        
+        # Do not train any layers of the VGG19 model, 
+        # So, set trainable to false. 
+        
         for layer in self.model.layers:
             layer.trainable = False
+        
+        """compute our content and style feature representations.
 
-        # Get the style and content feature representations (from our specified intermediate layers) 
-        style_features, content_features = get_feature_representations(self.model, content_path, style_path)
-        gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
+        Load and preprocess both the content and style 
+        images from their path. Then feed them through the network to obtain
+        the outputs of the intermediate layers. 
+        
+        In order to access the intermediate layers corresponding to our style and 
+        content feature maps, we get the corresponding outputs by using the Keras 
+        Functional API to define our model with the desired output activations.
+        """
+        # Load and Preprocess our images  
+        content_image = tt.load_img(content_path)
+        content_image = tf.keras.applications.vgg19.preprocess_input(content_image)
+
+        style_image = tt.load_img(style_path)
+        style_image = tf.keras.applications.vgg19.preprocess_input(style_image)
+
+        # Batch compute content and style features
+        style_outputs = self.model(style_image)
+        content_outputs = self.model(content_image)
+
+        # Get the style and content feature representations from our model  
+        style_features = [style_layer[0] for style_layer in style_outputs[:self.num_style_layers]]
+        content_features = [content_layer[0] for content_layer in content_outputs[self.num_style_layers:]]
+        gram_style_features = [tt.gram_matrix(style_feature) for style_feature in style_features]
 
         # Set initial image
-        init_image = tt.load_and_process_img(self, content_path)
+        init_image = tt.load_img(content_path)
+        init_image = tf.keras.applications.vgg19.preprocess_input(init_image)
         init_image = tfe.Variable(init_image, dtype=tf.float32)
+            
         # Create our optimizer
         opt = tf.train.AdamOptimizer(learning_rate=5, beta1=0.99, epsilon=1e-1)
 
@@ -159,7 +130,9 @@ class StyleTransferModel:
           'loss_weights': loss_weights,
           'init_image': init_image,
           'gram_style_features': gram_style_features,
-          'content_features': content_features
+          'content_features': content_features,
+          'num_style_layers':self.num_style_layers,
+          'num_content_layers':self.num_content_layers  
         }
 
         # For displaying
@@ -185,7 +158,7 @@ class StyleTransferModel:
             if loss < best_loss:
                 # Update best loss and best image from total loss. 
                 best_loss = loss
-                best_img = deprocess_img(init_image.numpy())
+                best_img = tt.deprocess_img(init_image.numpy())
 
             if i % display_interval== 0:
                 start_time = time.time()
--- transfer_tools.py
+++ transfer_tools.py
@@ -1,6 +1,8 @@
 import os
 import tensorflow as tf
 from tensorflow.python.keras.models import model_from_json
+from tensorflow.python.keras.preprocessing import image as kp_image
+
 import numpy as np
 from PIL import Image # pillow for image manipulation
 import time
@@ -8,6 +10,7 @@ import functools
 import IPython.display
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+
 mpl.rcParams['figure.figsize'] = (10,10)
 mpl.rcParams['axes.grid'] = False
 
@@ -90,10 +93,6 @@ def gram_matrix(input_tensor):
     gram = tf.matmul(a, a, transpose_a=True)
     return gram / tf.cast(n, tf.float32)
 
-def load_and_process_img(path_to_img):
-    img = load_img(path_to_img)
-    img = tf.keras.applications.vgg19.preprocess_input(img)
-    return img
 
 def show_results(best_img, content_path, style_path, show_large_final=True):
     plt.figure(figsize=(10, 5))
@@ -112,4 +111,54 @@ def show_results(best_img, content_path, style_path, show_large_final=True):
         plt.imshow(best_img)
         plt.title('Output Image')
         plt.show()
+
+def compute_loss(model, loss_weights, init_image, gram_style_features, content_features, num_style_layers, num_content_layers):
+    """This function will compute the loss total loss.
+
+    Arguments:
+    model: The model that will give us access to the intermediate layers
+    loss_weights: The weights of each contribution of each loss function. 
+      (style weight, content weight, and total variation weight)
+    init_image: Our initial base image. This image is what we are updating with 
+      our optimization process. We apply the gradients wrt the loss we are 
+      calculating to this image.
+    gram_style_features: Precomputed gram matrices corresponding to the 
+      defined style layers of interest.
+    content_features: Precomputed outputs from defined content layers of 
+      interest.
+
+    Returns:
+    returns the total loss, style loss, content loss, and total variational loss
+    """
+    style_weight, content_weight = loss_weights
+
+    # Feed our init image through our model. This will give us the content and 
+    # style representations at our desired layers. Since we're using eager
+    # our model is callable just like any other function!
+    model_outputs = model(init_image)
+
+    style_output_features = model_outputs[:num_style_layers]
+    content_output_features = model_outputs[num_style_layers:]
+
+    style_score = 0
+    content_score = 0
+
+    # Accumulate style losses from all layers
+    # Here, we equally weight each contribution of each loss layer
+    weight_per_style_layer = 1.0 / float(num_style_layers)
+    for target_style, comb_style in zip(gram_style_features, style_output_features):
+        style_score += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
+
+    # Accumulate content losses from all layers 
+    weight_per_content_layer = 1.0 / float(num_content_layers)
+    for target_content, comb_content in zip(content_features, content_output_features):
+        content_score += weight_per_content_layer* get_content_loss(comb_content[0], target_content)
+
+    style_score *= style_weight
+    content_score *= content_weight
+
+    # Get total loss
+    loss = style_score + content_score 
+    return loss, style_score, content_score
+
 
